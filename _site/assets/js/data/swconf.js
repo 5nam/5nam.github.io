cd3ffbe3c21d223e599fbc3537eacbb2b502d37b
5nam <ohnam90@gmail.com>
2025-01-03 02:17:05 +0900

    블로그 디자인 변경

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1735838003',resources: [
+    cacheName: 'chirpy-1735838206',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
